d625032f454c88e64270639599a9d356ee363da2
Shibin Shibu <shibinsb01@gmail.com>
2022-07-07 23:35:23 +0530

    Add files via upload

---
+++ shibin.js
@@ -0,0 +1,15 @@
+function makeRed(){
+    document.body.style.backgroundColor="#ea4335";
+}
+
+function makeGreen(){
+    document.body.style.backgroundColor="#34a853";  
+}
+
+function makeBlue(){
+    document.body.style.backgroundColor="#4285f4";  
+}
+
+function makeYellow(){
+    document.body.style.backgroundColor="#fbbc05";  
+}
